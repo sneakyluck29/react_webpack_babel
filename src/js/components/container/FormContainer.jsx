eda1139a34339fc552dcd837385be0d636be37ef
sneakyluck29 <davidwhitcombtech@gmail.com>
2020-02-01 06:17:03 -0800

    react component created

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Input from "../presentational/Input.jsx";
 
 class FormContainer extends Component{
         constructor() {
             super();
         this.state = {
-            title: ""
+            seo_title: ""
         };
+        this.handleChange = this.handleChange.bind(this);
     }
-
+    handleChange(event) {
+            this.setState({ [event.target.id]: event.target.valueOf });
+    }
     render() {
+            const {seo_title} = this.state;
         return (
-            <form id="article-form"></form>
+            <form id="article-form">
+                <Input
+                    text="SEO title"
+                    label="seo_title"
+                    type="text"
+                    id="seo_title"
+                    value={seo_title}
+                    handleChange={this.handleChange}
+                    />
+            </form>
         );
     }
 
